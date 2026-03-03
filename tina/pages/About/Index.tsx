475cfdb9697a0244c0a1d5ba77b6c3e570360cb1
Elijan Mastnak <git@ejmastnak.com>
2026-03-03 10:04:53 +0100

    feat(ui): Implement About page

--- tina/pages/About/Index.tsx
+++ tina/pages/About/Index.tsx
@@ -2,6 +2,7 @@ import { useTina, tinaField } from "tinacms/dist/react";
 import LinkButton from '@tina/components/LinkButton.tsx'
 import { TinaMarkdown } from "tinacms/dist/rich-text";
 import PageWrapper from '@tina/shared/PageWrapper.tsx'
+import Wrapper from '@tina/shared/Wrapper.tsx'
 
 import type { MyAboutPageQuery, MyAboutPageQueryVariables } from "@tina/__generated__/types";
 type Props = {
@@ -20,20 +21,17 @@ export default function AboutPage(props: Props) {
 
   return (
     <PageWrapper>
-      <h1 data-tina-field={tinaField(aboutPage, "h1")} className="text-5xl mx-auto text-center">{aboutPage.h1}</h1>
+      <img className="object-cover w-full max-w-sm md:max-w-md mx-auto rounded-md" data-tina-field={tinaField(aboutPage, "image")} src={aboutPage.image} alt={aboutPage.imageAlt}/>
 
-      <img className="object-cover max-w-md mx-auto rounded-md" data-tina-field={tinaField(aboutPage, "image")} src={aboutPage.image} alt={aboutPage.imageAlt}/>
-
-      <div data-tina-field={tinaField(aboutPage, "body")} className="prose mx-auto text-center">
+      <div data-tina-field={tinaField(aboutPage, "body")} className="mt-8 md:mt-10 prose mx-auto text-center">
         <TinaMarkdown content={aboutPage.body} />
       </div>
 
-      <div className="w-fit mx-auto">
+      <div className="mt-8 w-fit mx-auto">
         <LinkButton href={aboutPage.cv}>
           {aboutPage.cvButtonText}
         </LinkButton>
       </div>
-
     </PageWrapper>
   );
 }
