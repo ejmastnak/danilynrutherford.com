3802c214a1d527cfd390a69c219008fd421dbc01
Elijan Mastnak <git@ejmastnak.com>
2026-03-05 08:26:30 +0100

    feat(ui): add placeholder Contact page
    Form currently disabled; final form factor of page, form behavior, etc.
    pending Danilyn's review and approval.

--- tina/collections/ui/pages/contact/index.ts
+++ tina/collections/ui/pages/contact/index.ts
@@ -41,6 +41,93 @@ export const ContactPageCollection: Collection = {
       name: "h1",
       label: "Heading",
       type: "string",
-    }
+    },
+    {
+      name: 'intro',
+      label: 'Introductory text',
+      type: 'rich-text',
+    },
+    {
+      name: 'contactFormHeading',
+      label: 'Contact Form Heading',
+      type: 'string',
+    },
+    {
+      name: 'contactForm',
+      label: 'Contact form fields',
+      type: 'object',
+      fields: [
+        {
+          name: 'contactFormNameLabel',
+          label: 'Name field label',
+          type: 'string',
+        },
+        {
+          name: 'contactFormNamePlaceholder',
+          label: 'Name field placeholder',
+          type: 'string',
+        },
+        {
+          name: 'contactFormMessageLabel',
+          label: 'Message field label',
+          type: 'string',
+        },
+        {
+          name: 'contactFormMessagePlaceholder',
+          label: 'Message field placeholder',
+          type: 'string',
+        },
+        {
+          name: 'contactFormEmailLabel',
+          label: 'Email field label',
+          type: 'string',
+        },
+        {
+          name: 'contactFormEmailPlaceholder',
+          label: 'Email field placeholder',
+          type: 'string',
+        },
+        {
+          name: 'contactFormSubmitButtonText',
+          label: 'Submit button text',
+          type: 'string',
+        },
+      ],
+    },
+    {
+      name: 'contactDirectlyHeading',
+      label: 'Contact us directly heading',
+      type: 'string',
+    },
+    {
+      name: 'contactDirectlyText',
+      label: 'Contact us directly text',
+      type: 'rich-text',
+    },
+    {
+      name: 'phoneText',
+      label: 'Phone text',
+      type: 'string',
+    },
+    {
+      name: 'phone',
+      label: 'Phone',
+      type: 'string',
+    },
+    {
+      name: 'emailText',
+      label: 'Email text',
+      type: 'string',
+    },
+    {
+      name: 'email',
+      label: 'Email',
+      type: 'string',
+    },
+    {
+      name: 'phoneMachineReadable',
+      label: 'Machine-readable phone',
+      type: 'string',
+    },
   ],
 };
