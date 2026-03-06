0d5020ebfb35b402ce664577fc18d8ba958864c8
Elijan Mastnak <git@ejmastnak.com>
2026-03-06 18:00:12 +0100

    chore: temporarily remove contact form

--- tina/pages/Contact/Index.tsx
+++ tina/pages/Contact/Index.tsx
@@ -62,84 +62,92 @@ export default function ContactPage(props: Props) {
     <PageWrapper>
       <h1 data-tina-field={tinaField(contactPage, "h1")} className="text-5xl">{contactPage.h1}</h1>
 
-      {/* Intro message */}
-      <div data-tina-field={tinaField(contactPage, "intro")} className="mt-8 prose text-gray-700 max-w-2xl">
-        <TinaMarkdown content={contactPage.intro} />
+      <div className="mt-4 prose">
+        You can contact Danilyn by email at <a className="hover:text-blue-600" href={`mailto:${contactPage.email}`}>{contactPage.email}</a>.
       </div>
 
-      {/* Contact form */}
-      <div className="mt-5 relative rounded-xl bg-gray-50 p-5 border border-gray-200 -m-5 overflow-hidden">
-
-        <h2 data-tina-field={tinaField(contactPage, "contactFormHeading")}className="text-3xl">{contactPage.contactFormHeading}</h2>
-
-        <form data-tina-field={tinaField(contactPage, "contactForm")} className="mt-4" onSubmit={submitForm}>
-
-          <div className="w-full max-w-xl">
-            <InputLabel htmlFor="name" value={contactPage.contactForm.contactFormNameLabel} />
-            <TextInput
-              id="name"
-              type="text"
-              className="inline-block w-72"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              placeholder={contactPage.contactForm.contactFormNamePlaceholder}
-              required
-            />
+      { false && 
+        <div className="">
+          {/* Intro message */}
+          <div data-tina-field={tinaField(contactPage, "intro")} className="mt-8 prose text-gray-700 max-w-2xl">
+            <TinaMarkdown content={contactPage.intro} />
           </div>
 
-          <div className="mt-5 w-full max-w-xl">
-            <InputLabel htmlFor="message" value={contactPage.contactForm.contactFormMessageLabel} />
-            <TextArea
-              id="message"
-              className="block w-full h-48 text-sm max-w-xl"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              placeholder={contactPage.contactForm.contactFormMessagePlaceholder}
-              required
-            />
+          {/* Contact form */}
+          <div className="mt-5 relative rounded-xl bg-gray-50 p-5 border border-gray-200 -m-5 overflow-hidden">
+
+            <h2 data-tina-field={tinaField(contactPage, "contactFormHeading")}className="text-3xl">{contactPage.contactFormHeading}</h2>
+
+            <form data-tina-field={tinaField(contactPage, "contactForm")} className="mt-4" onSubmit={submitForm}>
+
+              <div className="w-full max-w-xl">
+                <InputLabel htmlFor="name" value={contactPage.contactForm.contactFormNameLabel} />
+                <TextInput
+                  id="name"
+                  type="text"
+                  className="inline-block w-72"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder={contactPage.contactForm.contactFormNamePlaceholder}
+                  required
+                />
+              </div>
+
+              <div className="mt-5 w-full max-w-xl">
+                <InputLabel htmlFor="message" value={contactPage.contactForm.contactFormMessageLabel} />
+                <TextArea
+                  id="message"
+                  className="block w-full h-48 text-sm max-w-xl"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  placeholder={contactPage.contactForm.contactFormMessagePlaceholder}
+                  required
+                />
+              </div>
+
+              {/* Email */}
+              <div className="mt-5 w-full max-w-xl">
+                <InputLabel htmlFor="email" value={contactPage.contactForm.contactFormEmailLabel} />
+                <TextInput
+                  id="email"
+                  type="email"
+                  className="inline-block w-72"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder={contactPage.contactForm.contactFormEmailPlaceholder}
+                  required
+                />
+              </div>
+
+              <PrimaryButton className="mt-8" type="submit" >
+                {contactPage.contactForm.contactFormSubmitButtonText}
+              </PrimaryButton>
+
+              {feedbackMessage &&
+                <p className="mt-1 max-w-md text-yellow-800 font-medium">
+                  {feedbackMessage}
+                </p>
+              }
+
+            </form>
+
           </div>
 
-          {/* Email */}
-          <div className="mt-5 w-full max-w-xl">
-            <InputLabel htmlFor="email" value={contactPage.contactForm.contactFormEmailLabel} />
-            <TextInput
-              id="email"
-              type="email"
-              className="inline-block w-72"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder={contactPage.contactForm.contactFormEmailPlaceholder}
-              required
-            />
+          {/* Direct contact data */}
+          <div className="mt-16">
+            <h2 data-tina-field={tinaField(contactPage, "contactDirectlyHeading")} className="text-3xl" id="contact-directly">{contactPage.contactDirectlyHeading}</h2>
+            <div className="mt-4">
+              <div  data-tina-field={tinaField(contactPage, "contactDirectlyText")} className="prose">
+                <TinaMarkdown content={contactPage.contactDirectlyText} />
+              </div>
+              <ul className="mt-4 ml-5 list-disc prose">
+                <li data-tina-field={tinaField(contactPage, "emailText")}>{contactPage.emailText} <a className="hover:text-blue-600" href={`mailto:${contactPage.email}`}>{contactPage.email}</a></li>
+                <li data-tina-field={tinaField(contactPage, "phoneText")}>{contactPage.phoneText} <a className="hover:text-blue-600" href={`tel:${contactPage.phoneMachineReadable}`}>{contactPage.phone}</a> </li>
+              </ul>
+            </div>
           </div>
-
-          <PrimaryButton className="mt-8" type="submit" >
-            {contactPage.contactForm.contactFormSubmitButtonText}
-          </PrimaryButton>
-
-          {feedbackMessage &&
-            <p className="mt-1 max-w-md text-yellow-800 font-medium">
-              {feedbackMessage}
-            </p>
-          }
-
-        </form>
-
-      </div>
-
-      {/* Direct contact data */}
-      <div className="mt-16">
-        <h2 data-tina-field={tinaField(contactPage, "contactDirectlyHeading")} className="text-3xl" id="contact-directly">{contactPage.contactDirectlyHeading}</h2>
-        <div className="mt-4">
-          <div  data-tina-field={tinaField(contactPage, "contactDirectlyText")} className="prose">
-            <TinaMarkdown content={contactPage.contactDirectlyText} />
-          </div>
-          <ul className="mt-4 ml-5 list-disc prose">
-            <li data-tina-field={tinaField(contactPage, "emailText")}>{contactPage.emailText} <a className="hover:text-blue-600" href={`mailto:${contactPage.email}`}>{contactPage.email}</a></li>
-            <li data-tina-field={tinaField(contactPage, "phoneText")}>{contactPage.phoneText} <a className="hover:text-blue-600" href={`tel:${contactPage.phoneMachineReadable}`}>{contactPage.phone}</a> </li>
-          </ul>
         </div>
-      </div>
+      }
 
     </PageWrapper>
   );
