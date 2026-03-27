38a959c753d7a945d59f4694be2480a7c30e9e1f
Elijan Mastnak <git@ejmastnak.com>
2026-03-27 14:56:24 +0100

    Populate HomePage with Tina CMS data
    Switches from hardcoded content to content taken from CMS

--- tina/collections/ui/pages/index/index.ts
+++ tina/collections/ui/pages/index/index.ts
@@ -23,19 +23,187 @@ export const HomePageCollection: Collection = {
       description: CMS_HTML_HEAD_FIELD_DESCRIPTION, 
       type: "object",
       fields: [
-      {
-        name: "title",
-        label: "Title",
-        description: CMS_HTML_HEAD_TITLE_FIELD_DESCRIPTION,
-        type: "string",
-      },
-      {
-        name: "description",
-        label: "Description",
-        description: CMS_HTML_HEAD_DESCRIPTION_FIELD_DESCRIPTION,
-        type: "string",
-      },
+        {
+          name: "title",
+          label: "Title",
+          description: CMS_HTML_HEAD_TITLE_FIELD_DESCRIPTION,
+          type: "string",
+        },
+        {
+          name: "description",
+          label: "Description",
+          description: CMS_HTML_HEAD_DESCRIPTION_FIELD_DESCRIPTION,
+          type: "string",
+        },
       ],
+    },
+    {
+      name: 'heroImage',
+      label: 'Main image',
+      type: 'image',
+    },
+    {
+      name: 'heroImageAlt',
+      label: 'Main image alt text',
+      type: 'string',
+    },
+    {
+      name: 'h1',
+      label: 'Main page heading',
+      type: 'string',
+    },
+    {
+      name: 'aboutText',
+      label: 'About text',
+      description: 'A short paragraph introducing Danilyn and her work',
+      type: 'rich-text',
+    },
+    {
+      name: 'featuredBook',
+      label: 'Featured book',
+      description: 'This book is featured prominently on the home page',
+      type: 'reference',
+      collections: ['book'],
+      ui: {
+        optionComponent: (
+          props: {
+            title: string,
+          },
+          _internalSys: { path: string }
+        ) => {
+          return props.title;
+        }
+      }
+    },
+    {
+      name: 'featuredBookDescription',
+      label: 'Featured book description',
+      description: 'A paragraph or so description of the main featured book',
+      type: 'rich-text',
+    },
+    {
+      name: 'featuredBookLinkText',
+      label: 'Featured book link text',
+      type: 'rich-text',
+    },
+    {
+      name: 'academicBooksHeading',
+      label: 'Academic books heading',
+      type: 'string',
+    },
+    {
+      name: 'featuredAcademicBooks',
+      label: 'featuredAcademicBooks',
+      type: 'object',
+      list: true,
+      ui: {
+        itemProps: (item) => {
+          return { label: item.book.replace(/^tina\/content\/books\//, "").replace(/\.json$/, "") };
+        },
+      },
+      fields: [
+        {
+          name: 'book',
+          label: 'book',
+          type: 'reference',
+          collections: ['book'],
+          ui: {
+            optionComponent: (
+              props: {
+                title: string,
+              },
+              _internalSys: { path: string }
+            ) => {
+              return props.title;
+            }
+          }
+        }   
+      ],
+    },
+    {
+      name: 'featuredEssaysHeading',
+      label: 'Featured essays heading',
+      type: 'string',
+    },
+    {
+      name: 'featuredEssays',
+      label: 'Featured essays',
+      type: 'object',
+      list: true,
+      ui: {
+        itemProps: (essay) => {
+          return { label: essay.title };
+        },
+      },
+      fields: [
+        {
+          name: 'title',
+          label: 'Essay title',
+          type: 'string',
+        },
+        {
+          name: 'link',
+          label: 'Link to essay',
+          description: 'E.g., to the journal/publisher\'s page where the essay can be viewed',
+          type: 'string',
+        },
+        {
+          name: 'description',
+          label: 'Description of essay',
+          description: 'A few sentences describing the essay',
+          type: 'rich-text',
+        },
+      ],
+    },
+    {
+      name: 'praiseHeading',
+      label: 'Praise heading',
+      type: 'string',
+    },
+    {
+      name: 'featuredPraise',
+      label: 'Featured praise',
+      type: 'object',
+      list: true,
+      ui: {
+        itemProps: (praise) => {
+          return { label: praise.reviewer + " on " + praise.work};
+        },
+      },
+      defaultItem: {
+        review: "...consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
+        reviewer: "John Doe",
+        work: "Beautiful Mystery",
+      },
+      fields: [
+        {
+          name: 'review',
+          label: 'Review text',
+          type: 'rich-text',
+        },
+        {
+          name: 'reviewer',
+          label: "Reviewer",
+          description: 'The name of the person or entity giving the praise',
+          type: 'string',
+        },
+        {
+          name: 'work',
+          label: 'Work',
+          description: 'The work being reviewed, e.g. "Beautiful Mystery"',
+          type: 'string',
+        },
+      ],
+    },
+    {
+      name: 'eventsHeading',
+      label: 'Events heading',
+      type: 'string',
+    },
+    {
+      name: 'eventsLinkText',
+      label: 'Events link button text',
+      type: 'string',
     },
   ],
 };
--- tina/pages/Index/Index.tsx
+++ tina/pages/Index/Index.tsx
@@ -6,14 +6,12 @@ import Wrapper from '@tina/shared/Wrapper.tsx'
 import LinkButton from '@tina/components/LinkButton.tsx'
 
 import danilynMillieImage from '@/assets/img/uploads/danilyn-millie.jpg'
-import danilynMillieExtendedImage from '@/assets/img/uploads/danilyn-millie.jpg'
 
 import type { MyHomePageQuery, MyHomePageQueryVariables, Book, Event } from "@tina/__generated__/types";
 type Props = {
   variables: MyHomePageQueryVariables;
   data: MyHomePageQuery;
   query: string;
-  books: Array<Book>;
   events: Array<Event>;
 };
 
@@ -25,56 +23,6 @@ export default function HomePage(props: Props) {
   });
   const homePage = data.homePage;
 
-  const BEAUTIFUL_MYSTERY_ID = "tina/content/books/beautiful-mystery.json";
-  const featuredBook = props.books.find(book => book.id == BEAUTIFUL_MYSTERY_ID);
-  const otherBooks = props.books.filter(book => book.id != BEAUTIFUL_MYSTERY_ID).sort((a, b) => b.year - a.year);
-
-  const featuredEssays = [
-    {
-      title: "Emergency Rooms",
-      href: "https://www.degruyter.com/document/doi/10.36019/9781978841413-006/html?srsltid=AfmBOoqe-QXW-gfqyQCoMYsVytg6lfnbE4bbexLFGxiQqi2vFnl-JUBh",
-      body: "Who gets to be the expert when it comes to care? Mother knows best, goes one version of the saying. Should it really be up to the doctors to decide what’s wrong and what to do? But what happens when mother doesn’t know best? When that warm and caring role starts feeling like an awkward gorilla costume, soft on the outside, hot and itchy on the inside, a get-up that makes it hard to move, see, and breathe?",
-    },
-    {
-      title: "Becoming an Operating System",
-      href: "https://anthrosource.onlinelibrary.wiley.com/doi/full/10.1111/amet.13013",
-      body: "What is communicated in communication? Given our assumptions about language and personhood, we Americans tend to think we know. Yet for those of us who live in the company of people like my daughter, there isn’t a clear answer to this question. We dream of depths of meaning. But our loved ones have taught us that communication is just as much a matter of surfaces, of the sensations that swirl around a touch, a sound, or a sight. "
-    },
-    {
-      title: "Proximity to Disability",
-      href: "https://muse.jhu.edu/article/757552/pdf",
-      body: "Proximity can be overwhelming. Proximity can be particularly overwhelming in these modern times. When we become close to another person, we expect them to affirm our identity: to help us see ourselves for who we really are. This is as much the case for our disabled children as for lovers. But what happens when we don’t get the recognition we’ve been taught we need?",
-    },
-    {
-      title: "Kinky Empiricism",
-      href: "https://anthrosource.onlinelibrary.wiley.com/doi/full/10.1111/j.1548-1360.2012.01154.x",
-      body: "It is time for anthropology to reclaim the empirical. But this reclaiming must be accompanied by a rethinking of what empiricism means. What I affirm in this article is a kind of empiricism that builds on the singular power of anthropological ways of knowing the world. A kinky empiricism: kinky, like a slinky, twisting back on itself, but also kinky, like S and M and other queer elaborations of established scenarios, relationships, and things.",
-    },
-  ]
-
-  const featuredPraise = [
-    {
-      review: "…a highly sophisticated, polished, and at times dazzling piece of work.",
-      reviewer: "Webb Keane",
-      book: "Raiding the Land of the Foreigners",
-    },
-    {
-      review: "…anthropology at its very best.",
-      reviewer: "Thomas Blom Hansen",
-      book: "Laughing at Leviathan",
-    },
-    {
-      review: "…a deeply thoughtful and refreshingly programmatic book.",
-      reviewer: "Patricia Spyer",
-      book: "Living in the Stone Age",
-    },
-    {
-      review: "Beautifully written and deeply moving.",
-      reviewer: "Tanya Luhrmann",
-      book: "Beautiful Mystery",
-    },
-  ]
-
   const today = new Date()
   const N = 2
   const upcomingEvents = props.events.filter(e => new Date(e.date) >= today).sort((a, b) => a.date >= b.date ? 1 : -1);
@@ -83,45 +31,42 @@ export default function HomePage(props: Props) {
   return (
     <div>
 
-      <img className="mx-auto lg:hidden object-cover w-full h-[360px] sm:h-[440px] md:rounded-md" src={danilynMillieImage.src} alt="Danilyn and her daughter Millie"/>
-      <img className="mx-auto hidden lg:block object-cover w-full h-[800px] md:rounded-md" src={danilynMillieExtendedImage.src} alt="Danilyn and her daughter Millie"/>
+      <img data-tina-field={tinaField(homePage, "heroImage")} className="mx-auto object-cover w-full h-[360px] sm:h-[440px] lg:h-[700px] xl:h-[750px] md:rounded-md" src={homePage.heroImage} alt={homePage.heroImageAlt}/>
 
       <PageWrapper>
 
         <div className="pb-20 border-b border-gray-300 ">
 
-          {/* About blurb */}
-          <h1 className="mt-3 text-5xl sm:text-6xl mx-auto text-center">Danilyn Rutherford</h1>
+          <h1 data-tina-field={tinaField(homePage, "h1")} className="mt-3 text-5xl sm:text-6xl mx-auto text-center">{homePage.h1}</h1>
 
-          <div className="mt-8 md:mt-12 prose mx-auto text-center xxxs:text-left max-w-2xl">
-            Danilyn Rutherford is an anthropologist and author. She began her career as a visiting tutor at Goldsmiths College, before joining the faculty at the University of Chicago, then the University of California, Santa Cruz, where she was professor and chair. She has a passion for supporting other scholars, which she satisfies through her day job as President of the Wenner-Gren Foundation for Anthropological Research.
+          {/* About blurb */}
+          <div data-tina-field={tinaField(homePage, "aboutText")} className="mt-8 md:mt-12 prose mx-auto text-center xxxs:text-left max-w-2xl">
+            <TinaMarkdown content={homePage.aboutText} />
           </div>
 
         </div>
 
         {/* Featured book */}
         <div className="mt-24 flex flex-col md:flex-row gap-x-10 lg:gap-x-12">
-          <div className="">
-            <h1 className="text-5xl font-medium text-center md:text-left">Beautiful Mystery</h1>
-            <p className="mt-0.5 md:text-lg text-gray-700 text-center md:text-left">Living in a wordless world</p>
-            {/* <p className="mt-2 inline-flex items-center w-fit rounded-full bg-white border border-theme-orange px-2 py-1 text-xs font-medium text-theme-orange ring-1 ring-inset ring-theme-orange/10">Newly published 2025</p> */}
+          <div>
+            <h1 className="text-5xl font-medium text-center md:text-left">{homePage.featuredBook.title}</h1>
+            <p className="mt-0.5 md:text-lg text-gray-700 text-center md:text-left">{homePage.featuredBook.subtitle}</p>
 
             {/* Desktop about blurb */}
             <div className="hidden md:block mt-5">
-              <div className="prose">
-                A short introductory paragraph introducing Beautiful Mystery.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              <div data-tina-field={tinaField(homePage, "featuredBookDescription")} className="prose">
+                <TinaMarkdown content={homePage.featuredBookDescription} />
               </div>
-              <LinkButton className="mt-5" href="/books/beautiful-mystery">
-                New book: <span className="italic">Beautiful Mystery</span> (2025)
+              <LinkButton data-tina-field={tinaField(homePage, "featuredBookLinkText")} className="mt-5" href={`/books/${homePage.featuredBook._sys.filename}`}>
+                <TinaMarkdown content={homePage.featuredBookLinkText} />
               </LinkButton>
             </div>
 
           </div>
-          <a href="/books/beautiful-mystery" className="h-fit">
+          <a href={`/books/${homePage.featuredBook._sys.filename}`} className="h-fit">
             <img
-              src={featuredBook.image}
-              alt={featuredBook.imageAlt}
+              src={homePage.featuredBook.image}
+              alt={homePage.featuredBook.imageAlt}
               className="w-full mx-auto md:mx-0 max-w-md lg:max-w-lg object-cover h-fit rounded hover:outline hover:outline-1 hover:outline-gray-300 hover:shadow-md" 
             />
           </a>
@@ -130,32 +75,32 @@ export default function HomePage(props: Props) {
 
         {/* Mobile about blurb and link button */}
         <div className="md:hidden mt-8">
-          <div className="prose max-w-xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <div data-tina-field={tinaField(homePage, "featuredBookDescription")} className="prose max-w-xl">
+            <TinaMarkdown content={homePage.featuredBookDescription} />
           </div>
           <div className="mt-5 w-fit mx-auto">
-            <LinkButton href="/books/beautiful-mystery">
-              New book: <span className="italic">Beautiful Mystery</span> (2025)
+            <LinkButton data-tina-field={tinaField(homePage, "featuredBookLinkText")} className="mt-5" href={`/books/${homePage.featuredBook._sys.filename}`}>
+              <TinaMarkdown content={homePage.featuredBookLinkText} />
             </LinkButton>
           </div>
         </div>
 
-        {/* Other books */}
+        {/* Academic books */}
         <section className="mt-16 md:mt-20">
-          <h2 className="text-5xl font-medium text-center sm:text-left">Academic books</h2>
+          <h2 data-tina-field={tinaField(homePage, "academicBooksHeading")} className="text-5xl font-medium text-center sm:text-left">{homePage.academicBooksHeading}</h2>
           <ul className="mt-10 grid sm:grid-cols-3 gap-x-8 gap-y-12 mx-auto">
-            {otherBooks.map((book) => (
+            {homePage.featuredAcademicBooks.map((item) => (
               <li>
-                <a href={`/books/${book._sys.filename}`} className="h-fit">
+                <a href={`/books/${item.book._sys.filename}`} className="h-fit">
                   <img
-                    src={book.image}
-                    alt={book.imageAlt}
+                    src={item.book.image}
+                    alt={item.book.imageAlt}
                     className="w-64 sm:max-w-md object-cover w-full aspect-[2/3] mx-auto rounded-md hover:outline hover:outline-1 hover:outline-gray-300 hover:shadow-md"
                   />
                 </a>
                 <div className="mt-3 sm:mt-1.5 text-center">
-                  <p className="text-lg font-semibold">{book.title}</p>
-                  <p className="text-gray-600">{book.publisher}, {book.year}</p>
+                  <p className="text-lg font-semibold">{item.book.title}</p>
+                  <p className="text-gray-600">{item.book.publisher}, {item.book.year}</p>
                 </div>
               </li>
             ))}
@@ -164,16 +109,16 @@ export default function HomePage(props: Props) {
 
         {/* Selected essays */}
         <section className="mt-20 md:mt-24">
-          <h2 className="text-5xl font-medium">Selected Essays</h2>
+          <h2 data-tina-field={tinaField(homePage, "featuredEssaysHeading")} className="text-5xl font-medium">{homePage.featuredEssaysHeading}</h2>
           <ul className="mt-8 grid md:grid-cols-2 gap-8 mx-auto">
-            {featuredEssays.map((essay) => (
-              <a href={essay.href} className="">
+            {homePage.featuredEssays.map((essay) => (
+              <a href={essay.link}>
                 <li className="bg-gray-50 rounded-md py-4 px-6 border border-gray-100 hover:outline hover:outline-1 hover:outline-gray-200 hover:shadow-md">
-                  <p className="underline font-semibold hover:text-blue-600">
+                  <p data-tina-field={tinaField(essay, "title")} className="underline font-semibold hover:text-blue-600">
                     {essay.title}
                   </p>
-                  <div className="mt-2 prose">
-                    {essay.body}
+                  <div data-tina-field={tinaField(essay, "description")} className="mt-2 prose">
+                    <TinaMarkdown content={essay.description} />
                   </div>
                 </li>
               </a>
@@ -185,12 +130,14 @@ export default function HomePage(props: Props) {
       {/* Praise */}
       <section className="mt-12 bg-theme-darkgreen text-white py-24">
         <Wrapper>
-          <h2 className="text-5xl font-medium">Praise</h2>
+          <h2 data-tina-field={tinaField(homePage, "praiseHeading")} className="text-5xl font-medium">{homePage.praiseHeading}</h2>
           <ul className="mt-8 grid md:grid-cols-4 gap-8 mx-auto px-4">
-            {featuredPraise.map((praise) => (
+            {homePage.featuredPraise.map((praise) => (
               <li>
-                <p className="font-semibold">“{praise.review}”</p>
-                <p className="mt-3">— {praise.reviewer} on <span className="italic">{praise.book}</span></p>
+                <div data-tina-field={tinaField(praise, "review")} className="font-semibold">
+                  <TinaMarkdown content={praise.review} />
+                </div>
+                <p className="mt-3">— <span data-tina-field={tinaField(praise, "reviewer")}>{praise.reviewer}</span> on <span data-tina-field={tinaField(praise, "work")} className="italic">{praise.work}</span></p>
               </li>
             ))}
           </ul>
@@ -200,7 +147,7 @@ export default function HomePage(props: Props) {
       {/* Events */}
       <section>
         <PageWrapper>
-          <h2 className="text-5xl font-medium">Events</h2>
+          <h2 data-tina-field={tinaField(homePage, "eventsHeading")} className="text-5xl font-medium">{homePage.eventsHeading}</h2>
           <ul role="list" className="mt-8 lg:mt-12 flex flex-col gap-y-8 w-fit">
             {upcomingEvents.slice(0, N).map((event) => (
               <li key={event.id}><EventComponent event={event}/></li>
@@ -212,12 +159,11 @@ export default function HomePage(props: Props) {
               ))
             }
           </ul>
-          <LinkButton className="mt-5" href="/events" >
-            More events
+          <LinkButton data-tina-field={tinaField(homePage, "eventsLinkText")} className="mt-5" href="/events" >
+            {homePage.eventsLinkText}
           </LinkButton>
         </PageWrapper>
       </section>
-
 
     </div>
   );
